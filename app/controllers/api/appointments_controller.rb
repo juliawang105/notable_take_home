053ed20ac4d8717f9676200e4510e65ff5bfcd37
Julia Wang <juliawang105@gmail.com>
2021-03-07 11:01:34 -0800

    Edit controller

--- app/controllers/api/appointments_controller.rb
+++ app/controllers/api/appointments_controller.rb
@@ -1,6 +1,6 @@
 class Api::AppointmentsController < ApplicationController
   def index 
-    # @appointments = Appointment.includes(:patient, :physician).where(physician_id: params[:physician_id])
+    # @appointments = Appointment.includes(:patient, :physician).where(physician_id: params[:physician_id]).where('appointments.time.to_date = Time.now.to_date)
     # debugger
     @physician = Physician.includes(:patients, :appointments).find_by(id: params[:physician_id])
   end
